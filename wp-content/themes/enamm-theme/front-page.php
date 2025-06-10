904e2df5b238de03eb5edf417c1ef23665b981a6
Jose Pardo <josmar.pm19@gmail>
2025-06-10 06:35:10 -0500

    update0635100625

--- wp-content/themes/enamm-theme/front-page.php
+++ wp-content/themes/enamm-theme/front-page.php
@@ -98,8 +98,8 @@ get_header(); // Incluye el contenido de header.php
                         <div class="bloque2__slider--track">
                             <div class="bloque2__slider--card">
                                 <div class="bloque2__card--content"
-                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/tcg9egos8ao3poo3bm6v.webp"
-                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/tcg9egos8ao3poo3bm6v.webp">
+                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/vl4xio3v6pfebgxdhzou.png"
+                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/vl4xio3v6pfebgxdhzou.png">
                                     <h3>Carrera de Diseño Gráfico</h3>
                                     <p>Explora tu creatividad y conviértete en un experto visual.</p>
                                 </div>
@@ -107,8 +107,8 @@ get_header(); // Incluye el contenido de header.php
 
                             <div class="bloque2__slider--card">
                                 <div class="bloque2__card--content"
-                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/trpja609dypx1v7ldgfq.webp"
-                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/trpja609dypx1v7ldgfq.webp">
+                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/o4zvs6lbdd2jgz3su7en.png"
+                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/o4zvs6lbdd2jgz3su7en.png">
                                     <h3>Ingeniería de Software</h3>
                                     <p>Desarrolla soluciones tecnológicas innovadoras para el futuro.</p>
                                 </div>
