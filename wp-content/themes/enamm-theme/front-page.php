afee4c6d80004d3e68419b622711051674328ac2
Jose Pardo <josmar.pm19@gmail>
2025-06-10 06:37:17 -0500

    update0637100625

--- wp-content/themes/enamm-theme/front-page.php
+++ wp-content/themes/enamm-theme/front-page.php
@@ -91,15 +91,15 @@ get_header(); // Incluye el contenido de header.php
                 <h3 class="bloque2__slider--title">Conoce nuestras carreras</h3>
 
                 <div class="bloque2__slider--container">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/phfrqlfusszh4r17cyvo.webp" alt="Anterior" class="bloque2__slider--arrow bloque2__slider--arrow--left">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014490/wh4ij8lmsns2unwohzxf.webp" alt="Siguiente" class="bloque2__slider--arrow bloque2__slider--arrow--right">
+                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/vl4xio3v6pfebgxdhzou.png" alt="Anterior" class="bloque2__slider--arrow bloque2__slider--arrow--left">
+                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/o4zvs6lbdd2jgz3su7en.png" alt="Siguiente" class="bloque2__slider--arrow bloque2__slider--arrow--right">
 
                     <div class="bloque2__slider--wrapper">
                         <div class="bloque2__slider--track">
                             <div class="bloque2__slider--card">
                                 <div class="bloque2__card--content"
-                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/vl4xio3v6pfebgxdhzou.png"
-                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/vl4xio3v6pfebgxdhzou.png">
+                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/tcg9egos8ao3poo3bm6v.webp"
+                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/tcg9egos8ao3poo3bm6v.webp">
                                     <h3>Carrera de Diseño Gráfico</h3>
                                     <p>Explora tu creatividad y conviértete en un experto visual.</p>
                                 </div>
@@ -107,8 +107,8 @@ get_header(); // Incluye el contenido de header.php
 
                             <div class="bloque2__slider--card">
                                 <div class="bloque2__card--content"
-                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/o4zvs6lbdd2jgz3su7en.png"
-                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749555263/o4zvs6lbdd2jgz3su7en.png">
+                                    data-bg-image-mobile="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/trpja609dypx1v7ldgfq.webp"
+                                    data-bg-image-desktop="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/trpja609dypx1v7ldgfq.webp">
                                     <h3>Ingeniería de Software</h3>
                                     <p>Desarrolla soluciones tecnológicas innovadoras para el futuro.</p>
                                 </div>
