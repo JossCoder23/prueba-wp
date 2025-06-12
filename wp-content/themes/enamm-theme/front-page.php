2ffecd345dd7f33164575343b9f5b04d19e52df3
Jose Pardo <jose.pardo@upch.pe>
2025-06-12 18:14:00 -0500

    update0613120625

--- wp-content/themes/enamm-theme/front-page.php
+++ wp-content/themes/enamm-theme/front-page.php
@@ -10,50 +10,6 @@ get_header(); // Incluye el contenido de header.php
 
 <main>
 
-    <!-- <div class="slider-container">
-        <div class="slider-wrapper">
-            <div class="slider-track">
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/237/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 1</h2>
-                        <p>Descripción breve de la Card 1.</p>
-                    </div>
-                </div>
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/238/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 2</h2>
-                        <p>Descripción breve de la Card 2.</p>
-                    </div>
-                </div>
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/239/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 3</h2>
-                        <p>Descripción breve de la Card 3.</p>
-                    </div>
-                </div>
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/240/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 4</h2>
-                        <p>Descripción breve de la Card 4.</p>
-                    </div>
-                </div>
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/241/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 5</h2>
-                        <p>Descripción breve de la Card 5.</p>
-                    </div>
-                </div>
-                <div class="slider-card" data-bg-image="https://picsum.photos/id/242/800/600">
-                    <div class="card-content">
-                        <h2>Título Card 6</h2>
-                        <p>Descripción breve de la Card 6.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="slider-dots">
-        </div>
-    </div> -->
     <div class="slider-container">
         <div class="slider-wrapper">
             <div class="slider-track">
@@ -85,6 +41,7 @@ get_header(); // Incluye el contenido de header.php
         </div>
         <div class="slider-dots"></div>
     </div>
+
     <div class="bloque2">
         <div class="bloque2__container">
             <div class="bloque2__slider">
@@ -121,6 +78,7 @@ get_header(); // Incluye el contenido de header.php
             <div class="bloque2__imageDesktop"></div>
         </div> 
     </div>
+
     <div class="bloque3">
         <div class="bloque3background"></div>
         <div class="bloque3__container">
@@ -179,29 +137,7 @@ get_header(); // Incluye el contenido de header.php
             </div>
         </div>
     </div>
-    <!-- <div class="bloque4">
-        <div class="bloque4Container">
-            <div class="bloque4__left">
-                <h4>Curso</h4>
-                <h3>Capacitación y entrenamiento</h3>
-                <p>Somos un centro homologado por la Dirección General de Capitanías y Guardacostas, para impartir los cursos de especialidad marítima. Con la garantía y homologación de:</p>
-                <div class="bloque4__left--content">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014489/veva6lm3vtgyu6wrq3zt.webp" alt="">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014489/vo44ihfat9onegilhyhp.webp" alt="">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014489/xwiticwc9eyvgidqzpee.webp" alt="">
-                </div>
-            </div>
-            <div class="bloque4__right">
-                <div class="bloque4__right--wrapper">
-                    <div class="bloque4__right--card">
-                        <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014490/bzuwb6wj2pdb6aytvjre.webp" alt="">
-                    </div>
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/phfrqlfusszh4r17cyvo.webp" alt="Anterior" class="bloque4__slider--arrow bloque4__slider--arrow--left">
-                    <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014490/wh4ij8lmsns2unwohzxf.webp" alt="Siguiente" class="bloque4__slider--arrow bloque4__slider--arrow--right">
-                </div>
-            </div>
-        </div>
-    </div> -->
+
     <div class="bloque4">
         <div class="bloque4Container">
             <div class="bloque4__left">
@@ -228,6 +164,74 @@ get_header(); // Incluye el contenido de header.php
         </div>
     </div>
 
+    <div class="bloque5">
+        <div class="bloque5__container">
+            <div class="bloque5__left"></div>
+            <div class="bloque5__right"></div>
+        </div>
+    </div>
+
+    <div class="bloque6">
+        <div class="bloque6__container"></div>
+    </div>
+
+    <div class="bloque7">
+        <div class="bloque7__container">
+            <h3 class="bloque3__title">Programas de Posgrado</h3>
+            <div class="bloque3__slider">
+                <div class="bloque3__slider--wrapper">
+                    <div class="bloque3__slider--track">
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                                <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/vi1pg9jsiwmmsmzkzpkg.webp" alt="">
+                                <h3><span>Maestría en</span> <br> Gestión Naviera</h3>
+                                <button>Ingresar</button>
+                            </div>
+                        </div>
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                               <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014487/gbtq3flbxn5iyhd0kg8t.webp" alt="">
+                               <h3><span>Maestría en</span> <br> Administración Marítima Portuaria y Pesquera</h3>
+                               <button>Ingresar</button>
+                            </div>
+                        </div>
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                               <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/vh5w1d9fejsgdiserctf.webp" alt="">
+                               <h3><span>Doctorado en</span> <br> Ciencias Marítimas</h3>
+                               <button>Ingresar</button>
+                            </div>
+                        </div>
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                               <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/juegraqfddyuayvfi9cs.webp" alt="">
+                               <h3><span>Diplomado en</span> <br> Medio Ambiente y Derecho Marítimo</h3>
+                               <button>Ingresar</button>
+                            </div>
+                        </div>
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                               <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/bm6xvvf50pu24ahvchdo.webp" alt="">
+                               <h3><span>Diplomado en</span> <br> Gestión Marítima Portuaria y Pesquera</h3>
+                               <button>Ingresar</button>
+                            </div>
+                        </div>
+                        <div class="bloque3__slider--card">
+                            <div class="bloque3__slider--content">
+                               <img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749014488/lsc5ba1emu1mgafdxq5f.webp" alt="">
+                               <h3><span>Diplomado en</span> <br> Comercio y Finanzas Marítima Portuaria</h3>
+                               <button>Ingresar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button class="bloque3__slider--arrow bloque3__prev--arrow"><img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749623199/nm5llpdj22jw3dfgxblc.png" alt=""></button>
+                <button class="bloque3__slider--arrow bloque3__next--arrow"><img src="https://res.cloudinary.com/dpuerx2lr/image/upload/v1749623199/dhfmoox4hmjnkwknlioz.png" alt=""></button>
+                <div class="bloque3__slider--dots">
+                </div>
+            </div>
+        </div>
+    </div>
 
 </main>
 
